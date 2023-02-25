23fa5c8305e98a33b833c151a6470301218743c6
supremgy <supremgy@icloud.com>
2023-02-25 15:56:37 +0900

    bug fix

--- src/pages/ProtectedRoute.jsx
+++ src/pages/ProtectedRoute.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { useAuthContext } from "../context/AuthContext";
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
 export default function ProtectedRoute({ children, requireAdmin }) {
   const { user } = useAuthContext();
   if (!user || (requireAdmin && !user.isAdmin)) {
-    return <Navigate to="/" replace />;
+    return <Navigate to='/' replace />;
   }
 
   return children;
-  // 로그인한 사용자가 있는지 확인
-  // 그 사용자가 어드민 권한이 있는지 확인
-  // requireAmin이 trun인 경우에는 로그인도 되어 있어야 하고, 어드민 권한도 가지고 있어야 함
-  // 조건에 맞지 않으면/ 상위 경로로 이동
-  // 조건에 맞는 경우에만 전달된 children을 보여줌
 }
